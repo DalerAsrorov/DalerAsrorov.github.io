227418e5709891ce7c3f78ff6c3df8a078afb26a
DalerAsrorov <asrorids@gmail.com>
2020-07-22 01:59:21 -0400

    Updates

---
+++ component---src-pages-404-tsx-b0fab05cf4c10c2614f7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(n,o,i){"use strict";i.r(o);o.default=function(){return"undefined"!=typeof window&&(window.location="/"),null}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-b0fab05cf4c10c2614f7.js.map
