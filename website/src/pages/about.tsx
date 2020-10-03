1f4c8957c77522e8408272dca6a8671f39bc88cb
DalerAsrorov <asrorids@gmail.com>
2020-10-02 22:43:19 -0400

    fook the image

--- website/src/pages/about.tsx
+++ website/src/pages/about.tsx
@@ -5,30 +5,11 @@ import { FaGithubAlt, FaLinkedin, FaSoundcloud } from 'react-icons/fa';
 import { IconBaseProps } from 'react-icons/lib/cjs';
 import Layout from '../components/layout';
 import { PlainList } from '../components/plain-list';
-import myPicture from '../images/me.jpg';
 import { rhythm } from '../utils/typography';
 
-const Container = styled.div`
-  display: flex;
 
-  @media (max-width: 650px) {
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-const Card = styled.article`
-  display: inline-block;
-  align-items: center;
-  margin: 0 ${rhythm(1)} ${rhythm(1)} 0;
-  float: left;
-`;
-
-const Image = styled.img`
-  max-width: ${rhythm(8)};
-  margin-bottom: ${rhythm(1 / 4)};
-  border-radius: 50%;
-  border: 2px solid hotpink;
+const Card = styled.div`
+margin-bottom: ${rhythm(1 / 4)};
 `;
 
 interface IconWrapper extends IconBaseProps {
@@ -42,9 +23,7 @@ const IconWrapper: React.FC<IconWrapper> = ({ Icon, ...restProps }) => (
 const About: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <Container>
         <Card>
-          <Image src={myPicture} />
           <PlainList
             items={[
               {
@@ -65,7 +44,6 @@ const About: React.FC<PageProps> = () => {
           projects on and off as a way to learn new technologies and found it to
           be most useful way to understand complex concepts more thoroughly.
         </p>
-      </Container>
     </Layout>
   );
 };
